759c96c340aea8c5d93a8b09ea94a0bbf725cfd3
samlanth <samuellanthier@hotmail.com>
2018-04-30 18:46:57 -0400

    j

--- Operator 7.3/Source.cpp
+++ Operator 7.3/Source.cpp
@@ -55,3 +55,30 @@ int main()
 	/*CFraction f1(1, 2);
 	try */
 }
+int division(int a, int b) // Calcule a divisé par b.
+{
+	if (b == 0)
+		throw string("ERREUR : Division par zéro !");
+	else
+		return a / b;
+}
+
+int main2()
+{
+	int a, b;
+	cout << "Valeur pour a : ";
+	cin >> a;
+	cout << "Valeur pour b : ";
+	cin >> b;
+
+	try
+	{
+		cout << a << " / " << b << " = " << division(a, b) << endl;
+	}
+	catch (string const& chaine)
+	{
+		cerr << chaine << endl;
+	}
+	return 0;
+	system("pause");
+}
